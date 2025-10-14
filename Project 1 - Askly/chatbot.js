3930eae6771aa5dbb9704606858cd3fab4b2e7d7
MohitCode17 <mohit.codes17@gmail.com>
2025-10-14 22:48:47 +0530

    Project1 - Add chatbot server

---
+++ Project 1 - Askly/chatbot.js
@@ -0,0 +1,89 @@
+import Groq from "groq-sdk/index.mjs";
+import { tavily } from "@tavily/core";
+
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
+const tvly = tavily({ apiKey: process.env.TAVILY_API_KEY });
+
+export async function generate(userMessage) {
+  const messages = [
+    {
+      role: "system",
+      content: `You are a smart personal assistant who answers the asked questions.
+            You have access to following tools:
+            1. webSearch({query}: {query: string}) //Search the latest information and realtime data on the internet.
+            Current date and time: ${new Date().toUTCString()}`,
+    },
+  ];
+
+  messages.push({
+    role: "user",
+    content: userMessage,
+  });
+
+  while (true) {
+    const completion = await groq.chat.completions.create({
+      model: "llama-3.3-70b-versatile",
+      temperature: 0,
+      messages: messages,
+      tools: [
+        {
+          type: "function",
+          function: {
+            name: "webSearch",
+            description:
+              "Search the latest information and realtime data on the internet.",
+            parameters: {
+              type: "object",
+              properties: {
+                query: {
+                  type: "string",
+                  description: "The search query to perform search on.",
+                },
+              },
+              required: ["query"],
+            },
+          },
+        },
+      ],
+      tool_choice: "auto",
+    });
+
+    // WHEN RESULT COMES OF COMPLETION ONE PUSH INTO MESSAGES
+    messages.push(completion.choices[0].message);
+
+    let toolCalls = completion.choices[0].message.tool_calls;
+
+    if (!toolCalls) {
+      return completion.choices[0].message.content;
+    }
+
+    for (const tool of toolCalls) {
+      const functionName = tool.function.name;
+      const functionArgs = tool.function.arguments;
+
+      if (functionName === "webSearch") {
+        const toolResult = await webSearch(JSON.parse(functionArgs));
+
+        // PUSH THE TOOL CALL MESSAGE INTO MESSAGES ARRAY
+        messages.push({
+          tool_call_id: tool.id,
+          role: "tool",
+          name: functionName,
+          content: toolResult,
+        });
+      }
+    }
+  }
+}
+
+async function webSearch({ query }) {
+  console.log("Calling a web search...");
+
+  const response = await tvly.search(query);
+
+  const finalResult = response.results
+    .map((result) => result.content)
+    .join("\n\n");
+
+  return finalResult;
+}
---
+++ Project 1 - Askly/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import { generate } from "./chatbot.js";
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("OK");
+});
+
+app.post("/chat", async (req, res) => {
+  const { message } = req.body;
+
+  if (!message)
+    return res.status(400).json({ message: "Message field is required" });
+
+  const result = await generate(message);
+
+  res.status(200).json({ message: result });
+});
+
+app.listen(port, () => console.log(`Server running at port: ${port}`));
