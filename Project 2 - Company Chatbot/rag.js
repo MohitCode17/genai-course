484a341d2567882faedad273bcde5a709a72bda6
MohitCode17 <mohit.codes17@gmail.com>
2025-10-16 21:49:09 +0530

    Project2 - basic setup

---
+++ Project 2 - Company Chatbot/rag.js
@@ -0,0 +1,13 @@
+/**
+ * Implementation Plan
+ * Stage 1: Indexing
+ * 1. Load the document - pdf, text
+ * 2. Chunk the Document
+ * 3. Generate vector embedding
+ * 4. Store the vector embedding - Vector database
+ *
+ * State 2: Using the chatbot
+ * 1. Setup LLM
+ * 2. Add retrieval step
+ * 3. Pass input + relevant information to LLM
+ */
