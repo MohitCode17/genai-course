691b05651dc64683d8c96f07a4c7cec183a5c80d
MohitCode17 <mohit.codes17@gmail.com>
2025-10-17 09:48:49 +0530

    Project2 - chunk the doc

--- Project 2 - Company Chatbot/prepare.js
+++ Project 2 - Company Chatbot/prepare.js
@@ -1,7 +1,16 @@
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 
 export async function indexTheDocument(filePath) {
   const loader = new PDFLoader(filePath, { splitPages: false });
   const doc = await loader.load();
-  console.log(doc[0].pageContent);
+
+  const textSplitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 500,
+    chunkOverlap: 100,
+  });
+
+  const texts = await textSplitter.splitText(doc[0].pageContent);
+
+  console.log(texts);
 }
