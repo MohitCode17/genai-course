c52b80b8602c141672b31d262f16300edba49140
MohitCode17 <mohit.codes17@gmail.com>
2025-10-19 16:17:21 +0530

    generate vector embeddings and indexing

--- Project 2 - Company Chatbot/prepare.js
+++ Project 2 - Company Chatbot/prepare.js
@@ -1,5 +1,24 @@
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { OllamaEmbeddings } from "@langchain/ollama";
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { PineconeStore } from "@langchain/pinecone";
+import { Pinecone as PineconeClient } from "@pinecone-database/pinecone";
+
+// INITIALIZE OLLAMA EMBEDDINGS
+const embeddings = new OllamaEmbeddings({
+  model: "nomic-embed-text",
+});
+
+const pinecone = new PineconeClient({
+  apiKey: process.env.PINECONE_API_KEY,
+});
+
+const pineconeIndex = pinecone.Index(process.env.PINECONE_INDEX_NAME);
+
+const vectorStore = await PineconeStore.fromExistingIndex(embeddings, {
+  pineconeIndex,
+  maxConcurrency: 5,
+});
 
 export async function indexTheDocument(filePath) {
   const loader = new PDFLoader(filePath, { splitPages: false });
@@ -12,5 +31,12 @@ export async function indexTheDocument(filePath) {
 
   const texts = await textSplitter.splitText(doc[0].pageContent);
 
-  console.log(texts);
+  const documents = texts.map((chunk) => {
+    return {
+      pageContent: chunk,
+      metadata: doc[0].metadata,
+    };
+  });
+
+  await vectorStore.addDocuments(documents);
 }
