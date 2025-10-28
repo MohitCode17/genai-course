0c91bdf5d803677dc8e26ed8219ddfc37c76a7ff
MohitCode17 <mohit.codes17@gmail.com>
2025-10-28 21:48:47 +0530

    add custom tool calling to react agent

--- Project 3 - Askly React Agent/agent.js
+++ Project 3 - Askly React Agent/agent.js
@@ -1,6 +1,8 @@
 import { createReactAgent } from "@langchain/langgraph/prebuilt";
 import { ChatGroq } from "@langchain/groq";
 import { TavilySearch } from "@langchain/tavily";
+import { tool } from "@langchain/core/tools";
+import { json, z } from "zod";
 
 async function main() {
   const model = new ChatGroq({
@@ -13,16 +15,41 @@ async function main() {
     topic: "general",
   });
 
+  const calendarEvents = tool(
+    async ({ query }) => {
+      // GOOGLE CALENDAR LOGIC GOES HERE...
+      return JSON.stringify([
+        { title: "Meeting with Ritkit Shah", date: "28th Oct 2025", time: "2 PM", location: "Gmeet" },
+      ]);
+    },
+    // INFORMATION OF TOOL
+    {
+      name: "get-calendar-events",
+      description: "Call to get the calendar events.",
+
+      // SCHEMA TO VALIDATE THE QUERY
+      schema: z.object({
+        query: z
+          .string()
+          .describe("The query to use in your calendar event search."),
+      }),
+    }
+  );
+
   const agent = createReactAgent({
     llm: model,
-    tools: [search],
+    tools: [search, calendarEvents],
   });
 
   const result = await agent.invoke({
     messages: [
       {
+        role: "system",
+        content: `You are a personal assistant. Use provided tools to get the information if you don't have it. Current date and time: ${new Date().toUTCString()}`,
+      },
+      {
         role: "user",
-        content: "When was officially GPT-5 launched?",
+        content: "Do I have any meeting for tommorow?",
       },
     ],
   });
