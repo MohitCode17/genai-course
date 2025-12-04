3155a24fd8e81995374f8fafbfd5468273d106a7
MohitCode17 <mohit.codes17@gmail.com>
2025-12-04 22:14:18 +0530

    vector indexing the internal document

---
+++ Project 6 - Customer Support Chatbot/src/indexDocs.ts
@@ -0,0 +1,46 @@
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { OllamaEmbeddings } from "@langchain/ollama";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { PineconeStore } from "@langchain/pinecone";
+import { Pinecone as PineconeClient } from "@pinecone-database/pinecone";
+
+// INITIALIZE OLLAMA EMBEDDINGS
+const embeddings = new OllamaEmbeddings({
+  model: "nomic-embed-text",
+});
+
+const pinecone = new PineconeClient({
+  apiKey: process.env.PINECONE_API_KEY!,
+});
+
+const pineconeIndex = pinecone.Index(process.env.PINECONE_INDEX_NAME!);
+
+export const vectorStore = await PineconeStore.fromExistingIndex(embeddings, {
+  pineconeIndex,
+  maxConcurrency: 5,
+});
+
+export async function indexTheDocument(filePath: string) {
+  const loader = new PDFLoader(filePath, { splitPages: false });
+  const doc = await loader.load();
+
+  const textSplitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 500,
+    chunkOverlap: 100,
+  });
+
+  const texts = await textSplitter.splitText(doc[0].pageContent);
+
+  const documents = texts.map((chunk) => {
+    return {
+      pageContent: chunk,
+      metadata: doc[0].metadata,
+    };
+  });
+
+  await vectorStore.addDocuments(documents);
+
+  console.log("Done");
+}
+
+indexTheDocument("../cg-knowledge-base.pdf");
