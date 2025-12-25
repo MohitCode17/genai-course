c30c5f65fea4a469dbedb2d1f49e5302777a3313
MohitCode17 <mohit.codes17@gmail.com>
2025-12-25 23:12:25 +0530

    setup sse at server side

---
+++ Project 10 - Ai expense tracker/server/server.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "ok" });
+});
+
+app.get("/chat", (req, res) => {
+  // SSE (SERVER SENT EVENTS)
+  // 1. ADD SPECIAL HEADER
+  // 2. SEND DATA IN SPECIAL FORMAT
+
+  res.writeHead(200, {
+    "Content-Type": "text/event-stream",
+  });
+
+  setInterval(() => {
+    res.write("event: cgPing\n");
+    res.write("data: Happy coding\n\n");
+  }, 1000);
+
+  //   res.json({});
+});
+
+const PORT = process.env.PORT || 4100;
+
+app.listen(PORT, () =>
+  console.log(`Server is running on http://localhost:${PORT}`)
+);
